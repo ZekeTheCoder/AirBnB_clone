371b60ed982a31beb7d8179dc8097311e0f3f7a9
Thabang Maphangisane <thabangh647@gmail.com>
2024-02-13 17:07:50 +0200

    5. Store first object - create a unique FileStorage instance for the application

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,13 @@
+#!/usr/bin/python3
 """
 This is the docstring for the models package.
 
-It provides an overview of what the package contains and its purpose.
+import the FileStorage class from the models.engine.file_storage module
+and create a FileStorage instance named storage
+and calls the reload() method on this storage instance.
 """
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
